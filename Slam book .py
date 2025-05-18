42584363881d259e9053d74d910391f2dccbbdd6
AvinandanRout <champchandanrout@gmail.com>
2025-05-18 22:37:45 +0530

    SB

--- Slam book .py
+++ Slam book .py
@@ -45,5 +45,46 @@ def initial_slambook():
          def menu():
           print("****************************************************************************************"
           *****************")
+          print("\t\t\tSMARTPHONE DIRECTORY", flush=false)
+          print("******************************************************************************************************")
+          print("\t you can now perform the following operation on this slambook\n")
+          print("1. add a new contact")
+          print("6. Exit phonebook")
+
+          def add_contact(pb):
+           dip = []
+           for i in range(len(pb[0])):
+          
+         if i == 0:
+           di.append(str(input("Enter name:  ")))
+            if i == 1:
+           di.append(str(input("Enter number:  ")))
+            if i == 2:
+           di.append(str(input("Enter ge-mail adress:  ")))
+            if i == 3:
+           di.append(str(input("Enter date of birth(dd/mm/yy):  ")))
+            if i == 4:
+           di.append(str(input("Enter catagory(Family/Friends/work/Others:")))
+           pb.append(dip)
+           return pb
+
+           def thanks():
            
-        
+           print("******************************************************")
+           print("Thank you for using our slam book.")
+           print("Please visit again!")
+           print("***************************************************************************")
+           sys.exit("Bye Bye, have a nice day ahead!")
+
+           print("................................................................................")
+           print("Hello dear friends, welcome to out slam book")
+           print("You may now proceed to explore this slam book and fill your details about your friends")
+           print("...................................................................................................")
+           ch = 1
+           pb = initial_slambook()
+           while ch in (1, 2, 3 ,4 ,5):
+           ch = menu()
+           if ch == 1:
+           pb = add_contact(pb)
+           else:
+            thanks()
